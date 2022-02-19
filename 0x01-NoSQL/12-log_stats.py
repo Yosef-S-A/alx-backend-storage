13f1fff50a520c8d57661abd9604a6f639a2e6ae
Yosef-S-A <yosefsamuel22@gmail.com>
2022-02-18 22:19:24 -0800

    task

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-""" script that provides some stats about Nginx logs stored in MongoDB """
-
+""" Write a Python script that provides some stats about
+    Nginx logs stored in MongoDB
+"""
 from pymongo import MongoClient
 
 if __name__ == "__main__":
